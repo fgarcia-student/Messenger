0983425a480d5ac4aa58c507fa2a8db8da47cb8e
Francisco Garcia <fgarcia.student04@gmail.com>
2017-08-04 10:57:09 -0400

    setup for redux store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
+import reducers from './reducers';
 import 'athenaeum/lib/assets/styles.css';
 import './style/main.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
