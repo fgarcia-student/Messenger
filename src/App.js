dffd2f0e6317fc8fd0a4635a053f09713365676a
Francisco Garcia <fgarcia.student04@gmail.com>
2017-08-13 22:35:03 -0400

    prepared for client side routing

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Start from './components/start/start';
-// import Home from './components/home/home';
-
-const App = () => {
-  return <Start />
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import App from './App';
+import {BrowserRouter,Route, Switch} from 'react-router-dom';
+import promise from 'redux-promise';
+
+import Start from './components/start/start';
 import reducers from './reducers';
 import 'athenaeum/lib/assets/styles.css';
 import './style/main.css';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/" component={Start} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.getElementById('root'));
